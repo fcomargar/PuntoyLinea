83563bb77c78889b2597fc229d89b4745e8ed177
fcomargar <franciscomartinezgarcia@iestorredelrey.es>
2023-02-21 09:06:57 +0100

    Initial commit

---
+++ src/Punto.java
@@ -0,0 +1,9 @@
+public class Punto {
+    private double punto_x;
+    private double punto_y;
+
+    public Punto(double punto_x, double punto_y) {
+        this.punto_x = punto_x;
+        this.punto_y = punto_y;
+    }
+}
